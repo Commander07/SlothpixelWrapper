0e9cc8a52c7d57261c8d38e7d00b29747211e788
Commander07 <olledubbelw@gmail.com>
2021-03-27 23:53:12 +0000

    Started work on guild api

--- main.py
+++ main.py
@@ -16,18 +16,32 @@ class api:
             return requests.get(url=f"{baseurl}players/{ign}{'?key='+self.apikey if self.apikey is not None else ''}").json()
 
         def achievements(self, ign):
-            return requests.get(url=f"{baseurl}players/{ign}/achievements").json()
+            return requests.get(url=f"{baseurl}players/{ign}/achievements{'?key='+self.apikey if self.apikey is not None else ''}").json()
 
         def quests(self, ign):
-            return requests.get(url=f"{baseurl}players/{ign}/quests").json()
+            return requests.get(url=f"{baseurl}players/{ign}/quests{'?key='+self.apikey if self.apikey is not None else ''}").json()
 
         def recentGames(self, ign):
-            return requests.get(url=f"{baseurl}players/{ign}/recentGames").json()
+            return requests.get(url=f"{baseurl}players/{ign}/recentGames{'?key='+self.apikey if self.apikey is not None else ''}").json()
 
         def friends(self, ign):
-            return requests.get(url=f"{baseurl}players/{ign}/friends").json()
+            return requests.get(url=f"{baseurl}players/{ign}/friends{'?key='+self.apikey if self.apikey is not None else ''}").json()
 
         def status(self, ign):
-            return requests.get(url=f"{baseurl}players/{ign}/status").json()
+            return requests.get(url=f"{baseurl}players/{ign}/status{'?key='+self.apikey if self.apikey is not None else ''}").json()
+
+    class guild:
+
+        def __init__(self, apikey=None):
+            self.apikey = apikey
+
+        def get(self, ign: str, populatePlayers=None):
+            return requests.get(url=f"{baseurl}guilds/{ign}{'?key='+self.apikey if self.apikey is not None else ''}").json()
+
+        def name(self, name: str, populatePlayers=None):
+            return requests.get(url=f"{baseurl}guilds/name/{name}{'?key='+self.apikey if self.apikey is not None else ''}").json()
+
+        def id(self, id: str, populatePlayers=None):
+            return requests.get(url=f"{baseurl}guilds/id/{id}{'?key='+self.apikey if self.apikey is not None else ''}").json()
 
 slothpixel = api()
